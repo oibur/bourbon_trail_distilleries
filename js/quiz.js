54a42de1412486a2e3cc2d56e2b6c72da14e0835
Bryan McGill <bktmcgill@gmail.com>
2018-03-26 15:06:00 -0400

    added java quiz structure

---
+++ js/quiz.js
@@ -0,0 +1,45 @@
+var questions = [
+  ['How many states are in the United States?', 50],
+  ['How many continents are there?', 7],
+  ['How many legs does an insect have?', 6]
+];
+var correctAnswers = 0;
+var question;
+var answer;
+var response;
+var correct = [];
+var wrong = [];
+
+function print(message) {
+  var outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+function buildList(arr) {
+ var listHTML = '<ol>';
+  for (var i = 0; i < arr.length; i += 1) {
+    listHTML += '<li>' + arr[i] + '</li>';
+  }
+  listHTML += '</ol>';
+  return listHTML;
+}
+
+for (var i = 0; i < questions.length; i += 1) {
+  question = questions[i][0];
+  answer = questions[i][1];
+  response = prompt(question);
+  response = parseInt(response);
+  if (response === answer) {
+    correctAnswers += 1;
+    correct.push(question);
+  } else {
+    wrong.push(question);
+  }
+}
+
+html = "<h3><br>You got " + correctAnswers + "/10 questions right.<br>.</h3>"
+html += '<h2>These are the questions you got correct:</h2>';
+html += buildList(correct);
+html += '<h2>These are the questions I stumped you:</h2>';
+html += buildList(wrong);
+print(html);
