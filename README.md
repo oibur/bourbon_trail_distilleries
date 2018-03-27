c1dd6cc135dbf5262ab38304167f10d991e9014a
Bryan McGill <bktmcgill@gmail.com>
2018-03-26 23:27:25 -0400

    ALL DONE

--- README.md
+++ README.md
@@ -4,14 +4,10 @@
 ```
 My project is a website that offers information about Kectucky Bourbon distilleries and the Brands of Bourbon they distill.
 
-```
-
-
 
 ## Custom CSS Classes
 ```
-The class(es) I created are:
-
+The classes I created are:
 1. .logo
 .. which rounds the bourbon distillery logos a bit in the "Bourbon Distillery" section of my page. 
 
@@ -20,12 +16,8 @@ The class(es) I created are:
 ..which adds different background colors to each of the distillery brand cards.
 
 
-
 ## Custom JavaScript Functions
 ```
-The javascript functions I created are:
-
-1. function name ex( animate() )
-.. what function does ex( animate() is used to move the elements in the info div across the screen)
-
-```
+The javascript function I created is:
+1. function buildList(arr)
+.. which keeps tracks of which questions were answered correct and which were answered incorrect to be .pushed to html
--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,14 @@
 var questions = [
-  ['How many states are in the United States?', 50],
-  ['How many continents are there?', 7],
-  ['How many legs does an insect have?', 6]
+  ['true or false? Bourbon must be made in Kentucky', 'false'],
+  ['Bourbon can be distilled no higher than ___ proof', '160'],
+  ['Unaged Bourbon is often called _____ dog, or _____ lightning', 'white'],
+  ['Bourbon must be made from a mashbill of at least __% corn', '51'],
+  ['true or false? Bourbon must be aged in a barrel', 'false'], //any oak CONTAINER (could be a box, or even a mug!)//
+  ['Bottled in Bond Bourbon must be ___ proof', '100'],
+  ['For Bourbon, the most common substitute for rye is _____', 'wheat'],
+  ['__% of all Bourbon comes from Kentucky', '95'],
+  ['true or false? There are more barrels of Bourbon than people in Kentucky', 'true'],
+  ['___% of bourbon is whiskey', '100']
 ];
 var correctAnswers = 0;
 var question;
@@ -28,7 +35,6 @@ for (var i = 0; i < questions.length; i += 1) {
   question = questions[i][0];
   answer = questions[i][1];
   response = prompt(question);
-  response = parseInt(response);
   if (response === answer) {
     correctAnswers += 1;
     correct.push(question);
@@ -38,7 +44,7 @@ for (var i = 0; i < questions.length; i += 1) {
 }
 
 html = "<h3><br>You got " + correctAnswers + "/10 questions right.<br>.</h3>"
-html += '<h2>These are the questions you got correct:</h2>';
+html += '<h2>These are the questions you got right:</h2>';
 html += buildList(correct);
 html += '<h2>These are the questions I stumped you:</h2>';
 html += buildList(wrong);
